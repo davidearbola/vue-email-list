cd7ea1e381cb006e927fe2b5bd93bbcfe5c2b365
davidearbola <davidearbola@gmail.com>
2024-05-16 13:20:26 +0200

    minor fix

--- js/main.js
+++ js/main.js
@@ -1,38 +1,3 @@
-console.log(`Main collegato`);
-
-// const { createApp } = Vue;
-
-// createApp({
-// 	data() {
-// 		return {
-// 			listaMail: [],
-// 			inputValue: "",
-// 		};
-// 	},
-// 	methods: {
-// 		getData() {
-// 			for (let i = 1; i <= this.inputValue; i++) {
-// 				axios
-// 					.get(
-// 						"https://flynn.boolean.careers/exercises/api/random/mail"
-// 					)
-// 					.then((result) => {
-// 						let data = result.data;
-// 						this.listaMail.push(data.response);
-// 					});
-// 			}
-// 		},
-// 		clear() {
-// 			this.listaMail = [];
-// 			this.inputValue = "";
-// 		},
-// 	},
-// 	mounted() {
-// 		console.log("Applicazione montata");
-// 	},
-// }).mount("#app");
-
-//*********************************************************************** */
 const { createApp } = Vue;
 createApp({
 	data() {
